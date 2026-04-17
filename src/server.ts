56e7cfe96e0509fb736f4cd058b03e3567ae759f
popbojan <popbojan@yahoo.com>
2026-04-17 21:10:16 +0200

    Remove commented out redis code

--- src/server.ts
+++ src/server.ts
@@ -23,9 +23,7 @@ import { ValidateRefreshTokenActivity } from "./domain/auth/activity/validate-re
 import { JwtTokenAdapter } from "./adapters/driven/security/jwt-token.adapter";
 import { HmacOtpAdapter } from "./adapters/driven/security/hmac-otp.adapter";
 
-// import { redis } from "./conf/redis";
 import { CryptoAdapter } from "./adapters/driven/security/crypto.adapter";
-//import { RedisStoreAdapter } from "./adapters/driven/persistence/redis-store.adapter";
 import { InMemoryStoreAdapter } from "./adapters/driven/persistence/in-memory-store.adapter";
 
 const refreshTokenStore = new InMemoryStoreAdapter();
@@ -58,9 +56,6 @@ const cryptoPort = new CryptoAdapter();
 // --- Activities (Domain Services) ---
 const generateOtpActivity = new GenerateOtpActivity(otpPort);
 const generateTokenActivity = new GenerateTokenActivity(tokenPort);
-// const issueRefreshTokenActivity = new IssueRefreshTokenActivity(cryptoPort, redisStore);
-// const revokeRefreshTokenActivity = new RevokeRefreshTokenActivity(redisStore);
-// const validateRefreshTokenActivity = new ValidateRefreshTokenActivity(cryptoPort, redisStore);
 
 const issueRefreshTokenActivity = new IssueRefreshTokenActivity(cryptoPort, refreshTokenStore);
 const revokeRefreshTokenActivity = new RevokeRefreshTokenActivity(refreshTokenStore);
