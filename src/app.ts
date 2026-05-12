a1e6c903a762c355ad1ac4aa76f2dff537426621
popbojan <popbojan@yahoo.com>
2026-05-12 18:41:33 +0200

    Add new refactoring TODOs

--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,8 @@ import {DeleteTaskUseCase} from "./domain/task/delete-task.use-case.js";
 
 import {loadOpenApiRuntimeSpec} from "./adapters/driving/web/openapi/openapi-runtime-schema";
 
+// TODO: Define 403 statuses in SPEC when task for given token is not found
+// TODO: Refine update and get-by-task-id endpoints. 403 should be thrown when someone wants to update/get a task that doesn't belong to him (follow delete logic)
 // TODO: Cover all use-cases with Integration Tests (GET and DELETION[later])
 // TODO: Define Delete Task API
 // TODO: Add Linter and prettier
